a9af9fa0187d49478d02ab39b8525b01835779a3
Peter Racine Jr <peterracinejr@gmail.com>
2021-08-26 11:11:21 -0500

    finished phase 2

--- GuessingGame/Program.cs
+++ GuessingGame/Program.cs
@@ -8,10 +8,20 @@ namespace GuessingGame
     {
         static void Main(string[] args)
         {
+            List<int> Guesses = new List<int>();
+            int secretNumber = 42;
+
             Console.WriteLine("Guess the secret number");
             string answer = Console.ReadLine().ToLower();
             int userNumGuess = int.Parse(answer);
-            Console.Write($"your guess was {userNumGuess}");
+            // Console.Write($"your guess was {userNumGuess}");
+            if (userNumGuess == secretNumber) 
+            {
+                Console.WriteLine("You guessed correctly!");
+            } else 
+            {
+                Console.WriteLine("Wrong answer, sorry.");
+            }
         }
     }
 }
