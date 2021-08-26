8050f01734e57b7b00a6f46c75c26c55fb063a7c
Peter Racine Jr <peterracinejr@gmail.com>
2021-08-26 15:48:48 -0500

    Finished phase 7 & 8

--- GuessingGame/Program.cs
+++ GuessingGame/Program.cs
@@ -10,30 +10,82 @@ namespace GuessingGame
         {
             List<int> Guesses = new List<int>();
             Random rnd = new Random();
-            int secretNumber = rnd.Next(1, 101);
 
-            while (Guesses.Count < 4 ) 
+            Console.WriteLine(@"Difficulty options: 
+            ~Easy = (8 chances to guess) 
+            ~Medium = (6 chances to guess) 
+            ~Hard = (4 chances to guess)
+            ~Cheater = (Unlimited Chances to guess)");
+            Console.Write("Enter your difficulty choice: ");
+            string difficultyLevel = Console.ReadLine().ToLower();
+            int guessLimit;
+
+
+            if (difficultyLevel == "easy")
             {
-            Console.WriteLine("Guess the secret number. ");
-            Console.Write($"Remaining guesses: ({4 - (Guesses.Count)})");
-            string answer = Console.ReadLine().ToLower();
-            int userNumGuess = int.Parse(answer);
-            // Console.WriteLine($"{secretNumber}");
+                guessLimit = 7;
+            }
+            else if (difficultyLevel == "medium")
+            {
+                guessLimit = 5;
+            }
+            else if (difficultyLevel == "hard")
+            {
+                guessLimit = 3;
+            }
+            else 
+            {
+                guessLimit = int.MaxValue;
+            }
+
+
+            
+            // Console.Write($"Remaining guesses: ({4 - (Guesses.Count)})");
             // Console.Write($"your guess was {userNumGuess}");
-            if (secretNumber == userNumGuess) 
-            {
-                Console.WriteLine("You guessed correctly!");
-                break;
-            } else 
-            {
-                Guesses.Add(userNumGuess);
-                if (secretNumber < userNumGuess) 
+
+            Console.WriteLine("Guess the secret number. ");
+            // string answer = Console.ReadLine().ToLower();
+            int userNumGuess = 0; //= int.Parse(Console.ReadLine());
+            int secretNumber = rnd.Next(1, 101);
+            int guessCount = 0;
+            bool outOfGuesses = false;
+            Console.WriteLine($"{secretNumber}");
+
+
+            while (userNumGuess != secretNumber && !outOfGuesses)
                 {
-                    Console.WriteLine("Your answer is too high and is incorrect, sorry.");
+                    if (guessCount <= guessLimit)
+                    {
+                        if (secretNumber < userNumGuess) 
+                        {
+                        int guessesLeft = guessLimit - guessCount;
+                        userNumGuess = int.Parse(Console.ReadLine());
+                        guessCount++;
+                            Console.WriteLine($"Your answer is too high and is incorrect, sorry. You have {guessesLeft} guesses left. Enter your next guess: ");
+                        }
+                        else
+                        {
+                        int guessesLeft = guessLimit - guessCount;
+                        userNumGuess = int.Parse(Console.ReadLine());
+                        guessCount++;
+                            Console.WriteLine($"Your answer is too low and is incorrect, sorry. You have {guessesLeft} guesses left. Enter your next guess: ");
+                        }
+                    }
+                    else 
+                    {
+                        outOfGuesses = true;
+                    }    
                 }
-                else  Console.WriteLine("Wrong answer is too low and is incorrect, sorry.");
-            }
-            }
+                if (outOfGuesses) 
+                {
+                    Console.WriteLine("Your answer is incorrect and you're out of guess.");
+                }
+                else
+                {
+                    Console.WriteLine("You were right!");
+                }
         }
-    }
-}
+            
+    };
+};
+
